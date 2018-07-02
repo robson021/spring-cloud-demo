b2ea24b69942e13433728ff8f05d189c7b22f76c
rob <nowak021@gmail.com>
2018-07-02 23:46:26 +0200

    add currency service logic

---
+++ currency-service/src/main/java/demo/CurrencyExchangeRateController.java
@@ -0,0 +1,40 @@
+package demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/currency-exchange")
+public class CurrencyExchangeRateController {
+
+    private static final Logger log = LoggerFactory.getLogger(CurrencyExchangeRateController.class);
+
+    private final String currencyExchangeUrl;
+
+    private final String currencyToken;
+
+    private final RestTemplate restTemplate;
+
+    public CurrencyExchangeRateController(Environment env, RestTemplate restTemplate) {
+        this.currencyExchangeUrl = env.getProperty("currency-exchange-url");
+        this.currencyToken = env.getProperty("currency-token");
+        this.restTemplate = restTemplate;
+        log.info("currency exchange url: {}", currencyExchangeUrl);
+        log.info("currency token: {}", currencyToken);
+    }
+
+    @GetMapping("/{currency}")
+    public CurrencyExchangeDTO getExchangeRates(@PathVariable String currency) {
+        log.info("Request for exchange rates of currency: {}", currency);
+        String url = currencyExchangeUrl.replace(currencyToken, currency);
+        return restTemplate //
+                .getForEntity(url, CurrencyExchangeDTO.class)
+                .getBody();
+    }
+}
--- run_all.bat
+++ run_all.bat
@@ -1,7 +1,7 @@
 call mvnw.cmd clean compile
 start cmd /c mvnw.cmd spring-boot:run -pl config-server
 timeout 4
-start cmd /c mvnw.cmd spring-boot:run -pl eureka-server
-timeout 7
+::start cmd /c mvnw.cmd spring-boot:run -pl eureka-server
+::timeout 7
 start cmd /c mvnw.cmd spring-boot:run -pl weather-service
 start cmd /c mvnw.cmd spring-boot:run -pl currency-service
