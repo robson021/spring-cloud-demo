092d1efa1f71dfd8e17521d1c90a2ea378c835da
VWHD76 <robert.nowak@motorolasolutions.com>
2018-07-05 16:21:28 +0200

    add using random port and 2 instances of weather and currency services

--- run_all.bat
+++ run_all.bat
@@ -8,4 +8,9 @@ timeout 4
 start cmd /c mvnw.cmd spring-boot:run -pl eureka-server
 timeout 7
 start cmd /c mvnw.cmd spring-boot:run -pl weather-service
+timeout 1
+start cmd /c mvnw.cmd spring-boot:run -pl weather-service
+timeout 1
+start cmd /c mvnw.cmd spring-boot:run -pl currency-service
+timeout 1
 start cmd /c mvnw.cmd spring-boot:run -pl currency-service
