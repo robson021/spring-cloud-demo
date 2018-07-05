a2abff82d9c2bf190e8e0a16bfc3bca05e35f5f1
rob <nowak021@gmail.com>
2018-07-05 20:59:45 +0200

    fix build problems

--- run_all.bat
+++ run_all.bat
@@ -1,5 +1,5 @@
 :: build all modules
-call mvnw.cmd clean compile
+call mvnw.cmd clean install -DskipTests
 :: fail on maven error
 if %errorlevel% neq 0 exit /b %errorlevel%
 :: run all apps
@@ -8,9 +8,6 @@ timeout 4
 start cmd /c mvnw.cmd spring-boot:run -pl eureka-server
 timeout 7
 start cmd /c mvnw.cmd spring-boot:run -pl weather-service
-timeout 1
 start cmd /c mvnw.cmd spring-boot:run -pl weather-service
-timeout 1
 start cmd /c mvnw.cmd spring-boot:run -pl currency-service
-timeout 1
 start cmd /c mvnw.cmd spring-boot:run -pl currency-service
