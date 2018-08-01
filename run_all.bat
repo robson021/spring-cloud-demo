bca7995d217cbdc80c41808849dbbd3e40cfe3f3
VWHD76 <robert.nowak@motorolasolutions.com>
2018-08-01 16:33:02 +0200

    add gateway

--- run_all.bat
+++ run_all.bat
@@ -1,6 +1,4 @@
-:: build all modules
-call mvnw.cmd clean install -DskipTests
-:: fail on maven error
+call mvnw.cmd clean install -DskipTests -pl common
 if %errorlevel% neq 0 exit /b %errorlevel%
 :: run all apps
 start cmd /c mvnw.cmd spring-boot:run -pl config-server
@@ -8,10 +6,9 @@ timeout 4
 start cmd /c mvnw.cmd spring-boot:run -pl eureka-server
 timeout 12
 start cmd /c mvnw.cmd spring-boot:run -pl weather-service
-start cmd /c mvnw.cmd spring-boot:run -pl weather-service
 start cmd /c mvnw.cmd spring-boot:run -pl currency-service
-start cmd /c mvnw.cmd spring-boot:run -pl currency-service
-start cmd /c mvnw.cmd spring-boot:run -pl country-service
 start cmd /c mvnw.cmd spring-boot:run -pl country-service
 timeout 15
 start cmd /c mvnw.cmd spring-boot:run -pl info-service
+timeout 8
+start cmd /c mvnw.cmd spring-boot:run -pl gateway
--- run_config_and_discovery.bat
+++ run_config_and_discovery.bat
@@ -1,4 +1,6 @@
 call mvnw.cmd install -DskipTests -pl common
 start cmd /c mvnw.cmd spring-boot:run -pl config-server
-timeout 4
+timeout 6
 start cmd /c mvnw.cmd spring-boot:run -pl eureka-server
+timeout 6
+start cmd /c mvnw.cmd spring-boot:run -pl gateway
