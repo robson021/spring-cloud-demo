fcca023c9b61224bb88b20f531ae1b398b059a63
VWHD76 <robert.nowak@motorolasolutions.com>
2018-08-02 12:21:39 +0200

    add annotations

--- country-service/src/main/java/demo/CountryService.java
+++ country-service/src/main/java/demo/CountryService.java
@@ -4,12 +4,14 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CountryService {
 
--- gateway/src/main/java/demo/Gateway.java
+++ gateway/src/main/java/demo/Gateway.java
@@ -4,14 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.Order;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.server.WebExceptionHandler;
 import reactor.core.publisher.Mono;
 
 @SpringBootApplication
-@RestControllerAdvice
+@EnableDiscoveryClient
 public class Gateway {
 
     public static void main(String[] args) {
