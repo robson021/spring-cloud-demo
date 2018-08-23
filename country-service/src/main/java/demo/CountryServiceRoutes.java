2543f7be3b0603a909ce6ac3e8fd00c14e4a7f50
rob <nowak021@gmail.com>
2018-08-23 20:03:53 +0200

    add router functions

---
+++ country-service/src/main/java/demo/CountryServiceRoutes.java
@@ -0,0 +1,47 @@
+package demo;
+
+import demo.model.CountryDTO;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+
+@Configuration
+public class CountryServiceRoutes {
+
+    private final CountryRepository repository;
+
+    private final ModelMapper modelMapper;
+
+    public CountryServiceRoutes(CountryRepository repository, ModelMapper modelMapper) {
+        this.repository = repository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> routes() {
+        return RouterFunctions.route(GET("/country-info/all-countries"), this::getAllCountries)
+                .andRoute(GET("/country-info/{capitalCity}"), this::getCapitalCity);
+    }
+
+    private Mono<ServerResponse> getCapitalCity(ServerRequest request) {
+        return ServerResponse.ok().contentType(APPLICATION_JSON).body(getCountryInfo(request.pathVariable("capitalCity")), CountryDTO.class);
+    }
+
+    private Mono<CountryDTO> getCountryInfo(String capitalCity) {
+        return repository.findByCapitalCityContainingIgnoreCase(capitalCity)
+                .map(countryEntity -> modelMapper.map(countryEntity, CountryDTO.class))
+                .log();
+    }
+
+    private Mono<ServerResponse> getAllCountries(ServerRequest request) {
+        return ServerResponse.ok().contentType(APPLICATION_JSON).body(repository.findAll(), CountryEntity.class);
+    }
+}
