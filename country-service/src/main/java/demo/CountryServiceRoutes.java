898356e901b590139a6017599241ceaaf0f35a99
rob <nowak021@gmail.com>
2018-08-23 20:25:51 +0200

    refactor

--- country-service/src/main/java/demo/CountryServiceRoutes.java
+++ country-service/src/main/java/demo/CountryServiceRoutes.java
@@ -12,6 +12,7 @@ import reactor.core.publisher.Mono;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.ServerResponse.notFound;
 
 @Configuration
 public class CountryServiceRoutes {
@@ -26,22 +27,27 @@ public class CountryServiceRoutes {
     }
 
     @Bean
+    @SuppressWarnings("All")
     public RouterFunction<ServerResponse> routes() {
         return RouterFunctions.route(GET("/country-info/all-countries"), this::getAllCountries)
                 .andRoute(GET("/country-info/{capitalCity}"), this::getCapitalCity);
     }
 
+    private Mono<ServerResponse> getAllCountries(ServerRequest request) {
+        return ServerResponse.ok().contentType(APPLICATION_JSON)
+                .body(repository.findAll(), CountryEntity.class)
+                .switchIfEmpty(notFound().build());
+    }
+
     private Mono<ServerResponse> getCapitalCity(ServerRequest request) {
-        return ServerResponse.ok().contentType(APPLICATION_JSON).body(getCountryInfo(request.pathVariable("capitalCity")), CountryDTO.class);
+        return ServerResponse.ok().contentType(APPLICATION_JSON)
+                .body(getCountryInfo(request.pathVariable("capitalCity")), CountryDTO.class)
+                .switchIfEmpty(notFound().build());
     }
 
     private Mono<CountryDTO> getCountryInfo(String capitalCity) {
         return repository.findByCapitalCityContainingIgnoreCase(capitalCity)
                 .map(countryEntity -> modelMapper.map(countryEntity, CountryDTO.class))
-                .log();
-    }
-
-    private Mono<ServerResponse> getAllCountries(ServerRequest request) {
-        return ServerResponse.ok().contentType(APPLICATION_JSON).body(repository.findAll(), CountryEntity.class);
+                .doOnNext(countryDTO -> System.out.println("Found: " + countryDTO));
     }
 }
